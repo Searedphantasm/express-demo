c8a7e26ecbbfc41efe614ce73f05df59dd844af8
Searedphantasm <searedphantasm@gmail.com>
2024-08-19 03:55:10 -0700

    Refactoring index.js

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ require('dotenv').config();
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
+const logger = require('./middleware/logger');
 const config = require('config');
 const startupDebugger = require('debug')('app:startup');
 const helmet = require("helmet");
 const morgan = require("morgan");
-const home = require('./routes/home');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
+
+
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
 
 
+process.on('uncaughtException', (reason) => {
+    logger.error(reason.message, reason);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+    logger.error(reason.message, reason);
+    process.exit(1);
+});
 
 if (!process.env.JWT_SECRET) {
     console.error("FATAL ERROR: jwtPrivateKey is missing!");
@@ -37,23 +42,12 @@ app.use(express.static('public')); // css , image ,
 app.use(helmet());
 
 
-app.use('/api/genres', genres)
-app.use('/api/customers', customers)
-app.use('/api/movies', movies)
-app.use('/api/rentals', rentals)
-app.use('/', home);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
-
 console.log("Application Name:" + config.get('name'));
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('tiny'));
     startupDebugger("Using Morgan");
 }
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}...`));
