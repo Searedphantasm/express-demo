7b1677ad9d90952680f4eda497a5be1ddab7142a
Searedphantasm <searedphantasm@gmail.com>
2024-08-28 03:48:33 -0700

    Refactoring test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre')
 const {User} = require('../../models/user');
+const mongoose = require("mongoose");
 let server;
 
 describe('/api/genres', () => {
@@ -36,8 +37,15 @@ describe('/api/genres', () => {
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty('name', genre.name);
         });
-        it('should return 404 it invalid id is passed',async () => {
+
+        it('should return 404 if invalid id is passed',async () => {
             const res = await request(server).get('/api/genres/1');
+            expect(res.status).toBe(404);
+        });
+
+        it('should return 404 if no genre with the given id exists',async () => {
+            const id = new mongoose.Types.ObjectId();
+            const res = await request(server).get('/api/genres/' + id);
             expect(res.status).toBe(404);
         });
     });
